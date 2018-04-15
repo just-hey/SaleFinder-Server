bab94878911939ec70dcd1c1e21ae0658023d0d7
Justin Hays <jhays124@yahoo.com>
2018-04-15 13:58:04 -0700

    update to deploy

--- knexfile.js
+++ knexfile.js
@@ -28,7 +28,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_NAME,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: path.join(__dirname, 'db', 'migrations')
     },
