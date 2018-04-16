7a40ade03283509f35481780188bf19380fd4e78
Justin Hays <jhays124@yahoo.com>
2018-04-15 18:11:02 -0700

    deploy prep

---
+++ automated.js
@@ -0,0 +1,11 @@
+const axios = require('axios')
+let testURL = 'https://crawler-scrape.herokuapp.com/scrape/wholefoods/98177'
+
+function testFire() {
+  return axios.get('${testURL}')
+    .then(results => {
+      console.log(results)
+    })
+}
+
+testFire()
