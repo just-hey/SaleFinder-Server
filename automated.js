8ae0a0944ce0dfced87542056badd83e256db366
Justin Hays <jhays124@yahoo.com>
2018-04-15 18:33:18 -0700

    deploy prep

--- automated.js
+++ automated.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 let testURL = 'https://crawler-scrape.herokuapp.com/scrape/wholefoods/98177'
 
 function testFire() {
+  console.log('automated triggered!!!')
   return axios.get('${testURL}')
     .then(results => {
       console.log(results)
