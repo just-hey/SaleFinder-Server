2f23af319c18db2c09eb53edbd79463261d880d7
Justin Hays <jhays124@yahoo.com>
2018-04-17 15:17:47 -0700

    sms prep

--- automated.js
+++ automated.js
@@ -1,20 +1,4 @@
-// const axios = require('axios')
-// let testURL = 'https://crawler-scrape.herokuapp.com/scrape/kroger/98177'
-//
-// function testFire() {
-//   console.log('automated triggered!!!')
-//   return axios.get(`${testURL}`)
-//     .then(results => {
-//       console.log('worked!',results)
-//       return
-//     })
-//     .catch(err => {
-//       console.log('errored out',err)
-//       return
-//     })
-// }
-//
-// testFire()
+
 
 //get all saved cart_products per zip
 //if that string is found within any product in that zip >>> SMS
